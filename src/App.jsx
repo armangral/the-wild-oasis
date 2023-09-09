ccf875a4c66738c67d3d0635e084d7bce406306e
armangral <91218206+armangral@users.noreply.github.com>
2023-09-09 13:02:34 +0500

    setting styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-weight: 600;
+  font-size: 36px;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
+  color: white;
+`;
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <H1>Hello</H1>
+        <Button>Click</Button>
+      </div>
+    </>
+  );
 }
 
 export default App;
