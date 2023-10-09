81b153ae0a1437190e69b780b3f921fceb6a5b97
armangral <91218206+armangral@users.noreply.github.com>
2023-10-09 21:46:33 +0500

    uploading images to supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://fsvqywsiwnxxngzsiocb.supabase.co";
+export const supabaseUrl = "https://fsvqywsiwnxxngzsiocb.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzdnF5d3Npd254eG5nenNpb2NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQzNjM2MzgsImV4cCI6MjAwOTkzOTYzOH0.iTY6D7ZRyzpwpmKeEUKyAsyzzvW3UzeFHYiTGI7zMDA";
 const supabase = createClient(supabaseUrl, supabaseKey);
