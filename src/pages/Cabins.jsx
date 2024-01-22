18bb656c7603f10eaea1214478b8369f2cffdd0f
armangral <91218206+armangral@users.noreply.github.com>
2024-01-22 22:31:32 +0500

    making modal using compound components

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,7 +1,6 @@
 import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-
 import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
@@ -9,7 +8,7 @@ function Cabins() {
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter/Sort</p>
+        <p>Filter / Sort</p>
       </Row>
 
       <Row>
