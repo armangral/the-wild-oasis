87120ab9b9d07b02f54e6324cfc5242eb4c1794b
armangral <91218206+armangral@users.noreply.github.com>
2024-02-04 16:58:26 +0500

    adding error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./ui/ErrorFallback";
+import GlobalStyles from "./styles/GlobalStyles";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <ErrorBoundary
+    FallbackComponent={ErrorFallback}
+    onReset={() => window.location.replace("/")}
+  >
     <App />
-  </React.StrictMode>
+  </ErrorBoundary>
 );
